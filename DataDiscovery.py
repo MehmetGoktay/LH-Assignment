4a3250a8309d16bf602f3e30f5991c584957d38f
Mehmet Göktay <mehmetgoktay1989@gmail.com>
2025-12-10 11:57:56 +0100

    project added

---
+++ DataDiscovery.py
@@ -0,0 +1,145 @@
+import numpy as np
+import pandas as pd
+from pandas.core.config_init import pc_max_info_rows_doc
+from scipy.stats import ttest_ind, mannwhitneyu
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+
+def load_filter_impute_data(file_path):
+    # Read the CSV file and store it in a DataFrame
+    df = pd.read_csv(file_path)
+    # print(df.describe())
+    # print(df.isna().sum())
+    print(df.info())
+    # Fill empty values with NaN and drop hotels with missing review_scores
+    df["review_score"] = df["review_score"].replace(r"^\s*$", np.nan, regex=True)
+    df.loc[df["review_score"] < 0, "review_score"] = np.nan
+    df = df.dropna(subset=["review_score"])
+
+    # # Impute stars with mode
+    df["stars"] = df["stars"].replace(r"^\s*$", np.nan, regex=True)
+    df["stars"] = df["stars"].fillna(df["stars"].mode()[0])
+
+    # # Impute room_count with median
+    df["room_count"] = df["room_count"].replace(r"^\s*$", np.nan, regex=True)
+    df["room_count"] = df["room_count"].fillna(df["room_count"].median())
+    tweede = df.copy()
+    df["country"] = df["latitude"].apply(assign_country)
+    print(df.info())
+
+    return df
+
+# Assign country name based on latitude and longitude
+def assign_country(latitude):
+    if latitude > 51.5:
+        return "Netherlands"
+    else:
+        return "Belgium"
+
+# Calculate k-means clustering
+def kmeans_clustering(dataframe):
+
+    df = dataframe.copy()
+    features = ["review_score", "stars", "room_count", "latitude", "longitude"]
+    X = df[features].copy()
+    X_scaled = StandardScaler().fit_transform(X)
+
+    kmeans = KMeans(n_clusters=2, random_state=42)
+    df["cluster"] = kmeans.fit_predict(X_scaled)
+
+    centroids = df.groupby("cluster")[["review_score", "stars", "room_count", "latitude", "longitude"]].mean()
+    print(centroids)
+# Plot PCA
+def plot_pca(dataframe):
+    df = dataframe.copy()
+    features = ["review_score", "stars", "room_count", "latitude", "longitude"]
+    X = df[features]
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+
+    pca = PCA()
+    pca_components = pca.fit_transform(X_scaled)
+
+    df["PC1"] = pca_components[:, 0]
+    df["PC2"] = pca_components[:, 1]
+
+    plt.figure(figsize=(8, 6))
+    plt.scatter(df["PC1"], df["PC2"], c=df["country"].map({"Belgium": 0, "Netherlands": 1}), cmap="viridis", alpha=0.8)
+    plt.xlabel("PC1")
+    plt.ylabel("PC2")
+    plt.savefig("pca.png", dpi=300, bbox_inches="tight")
+
+
+if __name__ == "__main__":
+    # Load the data, perform data cleaning and imputation
+    df_hotels = load_filter_impute_data('/Users/nurkaraca/Desktop/Projects/Project-LH/hotels_information.csv')
+
+    # Create two dataframes for Belgium and The Netherlands
+    belgium_hotels = df_hotels[df_hotels["country"] == "Belgium"]
+    netherlands_hotels = df_hotels[df_hotels["country"] == "Netherlands"]
+
+    # T-test for review_score
+    t_stat, p_value = ttest_ind(belgium_hotels["review_score"], netherlands_hotels["review_score"], equal_var=False)
+    print('t-statistics', t_stat)
+    print('p-value', p_value)
+
+    # Mann-Whitney U test for stars
+    u_stat, p_value = mannwhitneyu(belgium_hotels["stars"], netherlands_hotels["stars"], alternative="two-sided")
+    print('u-statistics', u_stat)
+    print('p-value', p_value)
+
+    # Mann-Whitney U test for room count
+    u_stat, p_value = mannwhitneyu(belgium_hotels["room_count"], netherlands_hotels["room_count"], alternative="two-sided")
+    print('u-statistics', u_stat)
+    print('p-value', p_value)
+
+    # Performing correlation (Spearman) for Belgium and The Netherlands
+    corr_spearman = df_hotels.groupby("country")[["review_score", "stars", "room_count"]].corr(method="spearman")
+    print(corr_spearman)
+
+    # Kmeans clustering
+    kmeans_clustering(df_hotels)
+
+    # PCA plotting
+    plot_pca(df_hotels)
+
+    # ### What are the distributions of hotels when it comes to stars and review scores?
+
+    # Plot distributions of star ratings per country
+    plt.figure(figsize=(10,5))
+    sns.countplot(data=df_hotels, x="stars", hue="country", palette="Set2")
+    plt.title("Distribution of Star Ratings per Country")
+    plt.xlabel("Star Rating")
+    plt.ylabel("Number of Hotels")
+    plt.savefig("star_ratings_per_country.png", dpi=300, bbox_inches="tight")
+
+    # Plot review score distribution per country
+    plt.figure(figsize=(10,5))
+    sns.histplot(data=df_hotels, x="review_score", kde=True, hue="country", bins=15, palette="Set2", alpha=0.5)
+    plt.title("Review Score Distribution per Country")
+    plt.xlabel("Review Score")
+    plt.ylabel("Number of Hotels")
+    plt.savefig("review_scores_per_country.png", dpi=300, bbox_inches="tight")
+
+    # Plot distribution of room counts
+    plt.figure(figsize=(10,5))
+    sns.histplot(data=df_hotels, x="room_count", kde=True, bins=15, color="teal", alpha=0.6)
+    plt.title("Distribution of Room Counts")
+    plt.xlabel("Room Count")
+    plt.ylabel("Number of Hotels")
+    plt.savefig("distribution_of_room_counts_all.png", dpi=300, bbox_inches="tight")
+
+    # Plot room count distribution per country
+    plt.figure(figsize=(10,5))
+    sns.histplot(data=df_hotels, x="room_count",kde=True,bins=15,hue="country",alpha=0.5,palette="Set2")
+    plt.title("Room Count Distribution per Country")
+    plt.xlabel("Room Count")
+    plt.ylabel("Number of Hotels")
+    plt.savefig("distribution_of_room_counts_per_country.png", dpi=300, bbox_inches="tight")
+
---
+++ MarketsComparison.py
@@ -0,0 +1,114 @@
+import pandas as pd
+import numpy as np
+from DataDiscovery import assign_country
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from DataDiscovery import load_filter_impute_data
+import seaborn as sns
+
+############################ How many hotels are available per day per market? #############################
+
+# Loading pricing info
+bookings = pd.read_csv('/Users/nurkaraca/Desktop/Projects/Project-LH/pricing_data.csv')
+bookings["arrival_date"] = pd.to_datetime(bookings["arrival_date"])
+
+# Load hotel info
+hotels_raw = pd.read_csv('/Users/nurkaraca/Desktop/Projects/Project-LH/hotels_information.csv')
+
+# Assign each hotel to Belgium or the Netherlands
+hotels_raw["country"] = hotels_raw["latitude"].apply(assign_country)
+
+# Keep only columns needed for merging
+hotels = hotels_raw[["our_hotel_id", "country"]]
+
+# 3. Merge hotel metadata into bookings dataset
+df = bookings.merge(hotels, on="our_hotel_id")
+
+# 4. Compute daily hotel availability per market
+availability = (
+    df[df["is_sold_out"] == False]
+    .groupby(["arrival_date", "country"])["our_hotel_id"]
+    .nunique()
+    .reset_index(name="available_hotels")
+)
+
+availability["arrival_date"] = pd.to_datetime(availability["arrival_date"])
+
+# Plot
+plt.figure(figsize=(12, 6))
+
+for country in availability["country"].unique():
+    subset = availability[availability["country"] == country]
+    plt.plot(
+        subset["arrival_date"],
+        subset["available_hotels"],
+        label=country,
+        linewidth=2
+    )
+
+# Set a 7 day step to avoid overlapping xlabs
+ax = plt.gca()
+ax.xaxis.set_major_locator(mdates.DayLocator(interval=7))
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+
+plt.xticks(rotation=45)
+plt.xlabel("Arrival Date")
+plt.ylabel("Available Hotels")
+plt.title("Hotel Availability Per Day Per Market")
+plt.legend(title="Country")
+plt.grid(alpha=0.3)
+plt.tight_layout()
+
+# Save
+plt.savefig("hotel_availability_per_market.png", dpi=300, bbox_inches="tight")
+
+################# What are the overall pricing behaviors? Are there different patterns to be observed? #################
+def clean_pricing(df):
+    # Replace empty strings with NaN
+    df["price_value_ref"] = df["price_value_ref"].replace(r"^\s*$", np.nan, regex=True)
+    df["price_value_non_ref"] = df["price_value_non_ref"].replace(r"^\s*$", np.nan, regex=True)
+
+    # Convert to numeric (invalid entries → NaN)
+    df["price_value_ref"] = pd.to_numeric(df["price_value_ref"], errors="coerce")
+    df["price_value_non_ref"] = pd.to_numeric(df["price_value_non_ref"], errors="coerce")
+
+    # Remove rows where BOTH prices are missing
+    df = df.dropna(subset=["price_value_ref", "price_value_non_ref"], how="all")
+
+    return df
+
+df_hotels = load_filter_impute_data('/Users/nurkaraca/Desktop/Projects/Project-LH/hotels_information.csv')
+df_bookings = pd.read_csv('/Users/nurkaraca/Desktop/Projects/Project-LH/pricing_data.csv')
+
+df_bookings = clean_pricing(df_bookings)
+
+df_merged = df_bookings.merge(
+    df_hotels[["our_hotel_id", "country"]],
+    on="our_hotel_id",
+    how="left"
+)
+
+# Convert refundable + non-refundable columns into long format
+price_df = df_merged.melt(
+    id_vars=["country"],                      # keep country
+    value_vars=["price_value_ref", "price_value_non_ref"],
+    var_name="price_type",
+    value_name="price"
+)
+
+plt.figure(figsize=(10,6))
+sns.boxplot(
+    data=price_df,
+    x="country",
+    y="price",
+    hue="price_type",
+    palette="Set2"
+)
+
+plt.title("Price Distribution per Market (Refundable vs Non-refundable)", fontsize=14)
+plt.xlabel("Country")
+plt.ylabel("Price")
+plt.grid(axis="y", alpha=0.3)
+plt.legend(title="Price type")
+plt.tight_layout()
+plt.savefig("price_distribution_per_market.png", dpi=300, bbox_inches="tight")
